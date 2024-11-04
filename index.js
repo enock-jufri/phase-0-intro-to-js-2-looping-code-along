244c1df958b87891ffecc2e88eb0251a61181d85
enock-jufri <enockjufri@gmail.com>
2024-11-04 11:45:42 +0300

    looping

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+const elem = ["Guadalupe", "Ollie", "Aki"]
+function writeCards(x,y){
+    const messages=[]
+    x.forEach(element => {
+        messages.push((`Thank you, ${element}, for the wonderful ${y} gift!`));
+    });
+    return messages
+}
+writeCards(["Guadalupe", "Ollie", "Aki"], "surprise")
+
+function countDown(x){
+while (x>=0){
+    console.log(x)
+    x--
+}
+}
+countDown(10)
